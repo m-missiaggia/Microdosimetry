cb5cf650a9db74303d2c2556ed56ed2913c632fd
m-missiaggia <marta.missiaggia@miami.edu>
2024-03-14 11:20:20 +0100

    Added comments and new paths.

--- SOI_Maestro_Analysis.R
+++ SOI_Maestro_Analysis.R
@@ -24,6 +24,9 @@ library(parallel)
 library(tidymodels)
 library(data.table)
 
+
+# I am defining the themes and color palettes that I will use in the plots
+
 theme_set(theme_bw()+theme(plot.title = element_text(size=20, color="black"),
                            axis.title.x = element_text(size=20, color="black"),
                            axis.title.y = element_text(size=20, color="black"),
@@ -52,64 +55,121 @@ c25 <- c(
   "darkorange4", "brown"
 )
 
+# Here you can put the path where you have your raw measurements. 
+# The measurements should be .Spe files from MAESTRO (ORTEC). 
 
-setwd("C:\\Users\\Utente\\Documents\\SOI_Trento\\13March2024\\")
+setwd("C:/Users/missi/OneDrive/Documents/Microdosimetry/14March2024_measurements")
+
+# Here you can put a first cut (noise threshold) that you want to avoid in the analysis
 
 cut <- 22
 
+# df is the dataframe of the file you want to analyze. Then I read the file,
+# skipping lines that containes different information that I don't use. 
+# I will use the counts, the row line is the ADC channel. 
+# I will also use the time information in the file.
+
 df <- fread("70MeV_muscle_15nA.Spe")
+
 count <- as.numeric(df$`$SPEC_ID:`[12:4107]) %>% 
   as.data.frame()
+
 count <- count %>% 
   mutate(channel = 1:nrow(count))
+
 colnames(count)[1] <- "count"
+
+# Now I am plotting the file similarly to a yd(y) distribution, but in ADC channels
+
 count$count2 <- count$count*count$channel^2
+
 count$Type <- "15nA"
 
+# same but for file number 2
+
 df2 <- fread("70MeV_muscle_40nA.Spe")
+
 count2 <- as.numeric(df2$`$SPEC_ID:`[12:4107]) %>% 
   as.data.frame()
+
 count2$channel <- 1:nrow(count2)
+
 colnames(count2)[1] <- "count"
+
 count2$count2 <- count2$count*count2$channel^2
+
 count2$Type <- "40nA"
+
 count2$count2 <- (count2$count2*max(count$count2[count2$channel > cut]))/max(count2$count2[count2$channel > cut])
 
+# same but for file number 3
+
 df3 <- fread("70MeV_muscle_30nA_1.Spe")
+
 count3 <- as.numeric(df3$`$SPEC_ID:`[12:4107]) %>% 
   as.data.frame()
+
 count3$channel <- 1:nrow(count3)
+
 colnames(count3)[1] <- "count"
+
 count3$count2 <- count3$count*count3$channel^2
-count3$Type <- "30nA"
+
+count3$Type <- "30nA - 1"
+
 count3$count2 <- (count3$count2*max(count$count2[count3$channel > cut]))/max(count3$count2[count3$channel > cut])
 
+# same but for file number 4
+
 df4 <- fread("70MeV_muscle_30nA_2.Spe")
+
 count4 <- as.numeric(df4$`$SPEC_ID:`[12:4107]) %>% 
   as.data.frame()
+
 count4$channel <- 1:nrow(count4)
+
 colnames(count4)[1] <- "count"
+
 count4$count2 <- count4$count*count4$channel^2
+
 count4$Type <- "30nA - 2"
+
 count4$count2 <- (count4$count2*max(count$count2[count4$channel > cut]))/max(count4$count2[count4$channel > cut])
 
+# same but for file number 5
+
 df5 <- fread("70MeV_muscle_30nA_3.Spe")
+
 count5 <- as.numeric(df5$`$SPEC_ID:`[12:4107]) %>% 
   as.data.frame()
 count5$channel <- 1:nrow(count5)
+
 colnames(count5)[1] <- "count"
+
 count5$count2 <- count5$count*count5$channel^2
+
 count5$Type <- "30nA - 3"
+
 count5$count2 <- (count5$count2*max(count$count2[count5$channel > cut]))/max(count5$count2[count5$channel > cut])
 
+# same but for file number 6
+
 df6 <- fread("70MeV_muscle_30nA_4.Spe")
+
 count6 <- as.numeric(df6$`$SPEC_ID:`[12:4107]) %>% 
   as.data.frame()
+
 count6$channel <- 1:nrow(count6)
+
 colnames(count6)[1] <- "count"
+
 count6$count2 <- count6$count*count6$channel^2
+
 count6$Type <- "30nA - 4"
+
 count6$count2 <- (count6$count2*max(count$count2[count6$channel > cut]))/max(count6$count2[count6$channel > cut])
+
+########## Plot of all measurements together #############
 
 p <- count %>% rbind(count2, count3, count4, count5, count6) %>% 
   filter(channel > cut) %>% 
@@ -118,11 +178,16 @@ p <- count %>% rbind(count2, count3, count4, count5, count6) %>%
   scale_x_log10() +
   scale_color_manual(values = cb_b) +
   # geom_vline(xintercept = 570, color = "maroon", linewidth = 1) +
-  xlab("Channel") + 
+  xlab("Channel"<) + 
   ylab("Counts")
-ggplotly(p)
-p
 
+ggplotly(p)
+
+
+
+
+
+##################### extra stuff #########################################
 
 
 sum(as.numeric(df4$`$SPEC_ID:`[12:4107]))/as.numeric(strsplit(df4$`$SPEC_ID:`[9]," ")[[1]][1]) 
